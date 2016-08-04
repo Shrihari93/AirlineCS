f77a96d65ac4d6ef4b32e9e4920c744676a7c980
Padmanabh Pande <pande.padmanabh@gmail.com>
2016-08-04 19:39:55 +0530

    Create BookServlet.java

---
+++ BookServlet.java
@@ -0,0 +1,49 @@
+package Entry;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class BookServlet
+ */
+@WebServlet("/BookServlet")
+public class BookServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public BookServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String flightID = request.getParameter("book");
+		RequestDispatcher rd = request.getRequestDispatcher("BookTicket.jsp");  //display flights
+		HttpSession s = request.getSession(true);
+		s.setAttribute("FlightID", flightID);
+		rd.forward(request, response);
+
+	}
+
+}
