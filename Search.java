a86f2e14ec56a2790d39fee397c2cfbb982a499c
Padmanabh Pande <pande.padmanabh@gmail.com>
2016-08-04 19:38:45 +0530

    Create Search.java

---
+++ Search.java
@@ -0,0 +1,28 @@
+package Entry;
+
+import java.sql.Date;
+
+public class Search {
+	private Date date;
+	private String source;
+	private String destination;
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public String getSource() {
+		return source;
+	}
+	public void setSource(String source) {
+		this.source = source;
+	}
+	public String getDestination() {
+		return destination;
+	}
+	public void setDestination(String destination) {
+		this.destination = destination;
+	}
+}
