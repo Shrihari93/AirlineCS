b6cc70200477f18d5a7b2fd46e4300fded282a89
Padmanabh Pande <pande.padmanabh@gmail.com>
2016-08-04 19:45:07 +0530

    Create ViewServlet.java

---
+++ ViewServlet.java
@@ -0,0 +1,49 @@
+package Entry;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class ViewServlet
+ */
+@WebServlet("/ViewServlet")
+public class ViewServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String PNR = request.getParameter("view");
+		RequestDispatcher rd = request.getRequestDispatcher("ViewTicket.jsp");  //display flights
+		HttpSession s = request.getSession(true);
+		s.setAttribute("Booking", PNR);
+		rd.forward(request, response);
+
+	}
+
+}
